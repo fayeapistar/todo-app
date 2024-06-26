f56f34a2dec8c3a386c0d7c5495a86eeedab3d8b
fayeapistar <161134127+fayeapistar@users.noreply.github.com>
2024-06-26 14:40:15 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Display a listing of the tasks
+
 Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
 
-// Show the form for creating a new task
 Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
 
-// Store a newly created task in storage
 Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
 
-// Display the specified task
 Route::get('tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
 
-// Show the form for editing the specified task
 Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
 
-// Update the specified task in storage
 Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
 
-// Remove the specified task from storage
 Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
